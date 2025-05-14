0efc3df74567a1532675b7d0d8d4111e0229796a
Nate <nathanael.paul.lewis@gmail.com>
2025-05-14 17:34:48 -0500

    Updated files on 2025-05-14 17:34:48

--- blender/installBlender.ps1
+++ blender/installBlender.ps1
@@ -1,6 +1,5 @@
 # Installs Blender and sets up work environment.
 
-$verOld = "4.2"
 $ver = "4.4"
 $verSub = ".3"
 $blenderPath = "$HOME/programs"
@@ -10,9 +9,8 @@ $webRequest = "https://ftp.blender.org/release/Blender$ver/$package$extension"
 $configPath = "$env:APPDATA/Blender Foundation/Blender"
 $dropboxConfigFiles = "https://www.dropbox.com/scl/fi/3mp73hnzl4eyuu5b4628g/configFiles.zip?rlkey=1snswsc65ouexugsmyj1gymum&st=slw6e39a&dl=1"
 
-
 function InstallBlender {
-  Write-Output "Downloading Blender..."
+  Write-Output "Downloading Blender $ver..."
   Invoke-WebRequest -Uri "$webRequest" -OutFile "$blenderPath/"
 
   Write-Output "Extracting Blender..."
@@ -27,8 +25,8 @@ function InstallBlender {
   Expand-Archive "$blenderPath/configFiles.zip" "$blenderPath/."
   Copy-Item -Path "$blenderPath/configFiles" -Destination "$configPath/$ver" -Force -Recurse
 
-  Write-Output "Launching Blender..."
-  Start-Process "$blenderPath/Blender$ver/blender.exe"
+  # Write-Output "Launching Blender $ver..."
+  # Start-Process "$blenderPath/Blender$ver/blender.exe"
 
   Write-Output "Removing installation files..."
   Remove-Item -Path "$blenderPath/$package$extension" -Force
@@ -39,15 +37,26 @@ function InstallBlender {
   }
 
 function ConfigPaths {
-  $blenderRoot = "$blenderPath/Blender$ver/$ver"
+  # $blenderRoot = "$blenderPath/Blender$ver/$ver"
+  #
+  # $env:BLENDER_USER_CONFIG = "$blenderRoot/config"
+  # $env:BLENDER_USER_DATAFILES = "$blenderRoot/datafiles"
+  # $env:BLENDER_USER_SCRIPTS = "$blenderRoot/scripts"
+  # $env:BLENDER_USER_EXTENSIONS = "$blenderRoot/extensions"
+  
+  $BlenderExePath = "$blenderPath/Blender4.4/blender.exe"  # Replace with the actual path to your blender.exe
+  $ShortcutPath = "$blenderPath/Blender4.4/Blender Portable.lnk" # Replace with where you want to save the shortcut
 
-  $env:BLENDER_USER_CONFIG = "$blenderRoot/config"
-  $env:BLENDER_USER_DATAFILES = "$blenderRoot/datafiles"
-  $env:BLENDER_USER_SCRIPTS = "$blenderRoot/scripts"
-  $env:BLENDER_USER_EXTENSIONS = "$blenderRoot/extensions"
+  $WScriptShell = New-Object -ComObject WScript.Shell
+  $Shortcut = $WScriptShell.CreateShortcut($ShortcutPath)
+  $Shortcut.TargetPath = $BlenderExePath
+  $Shortcut.Arguments = "--config-name ."
+  $Shortcut.Save()
 
+  Write-Host "Shortcut created at: $ShortcutPath"
+  Write-Host "Run Blender using this shortcut to use portable configuration."
   }
 
 InstallBlender
-# ConfigPaths
+ConfigPaths
 

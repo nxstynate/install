9b6cbc809865ab3be685619955680a44a4f1ac38
Nate <nathanael.paul.lewis@gmail.com>
2025-05-13 23:44:48 -0500

    Updated files on 2025-05-13 23:44:48

--- blender/installBlender.ps1
+++ blender/installBlender.ps1
@@ -8,6 +8,8 @@ $extension = ".zip"
 $package = "blender-$ver$verSub-windows-x64"
 $webRequest = "https://ftp.blender.org/release/Blender$ver/$package$extension"
 $configPath = "$env:APPDATA/Blender Foundation/Blender"
+$dropboxConfigFiles = "https://www.dropbox.com/scl/fi/3mp73hnzl4eyuu5b4628g/configFiles.zip?rlkey=1snswsc65ouexugsmyj1gymum&st=slw6e39a&dl=1"
+
 
 function InstallBlender {
   Write-Output "Downloading Blender..."
@@ -18,20 +20,23 @@ function InstallBlender {
 
   Set-Location $blenderPath/
   Write-Output "Renaming Folder..."
-
   Move-Item -Path "$package" -Destination "Blender$ver" -Force 
+
   Write-Output "Copying config files to setup your envinronment..."
-  Copy-Item -Path "$configPath/$verOld" -Destination "$configPath/$ver" -Force -Recurse
+  Invoke-WebRequest -Uri "$dropboxConfigFiles" -OutFile "$blenderPath/configFiles.zip"
+  Expand-Archive "$blenderPath/configFiles.zip" "$blenderPath/."
+  Copy-Item -Path "$blenderPath/configFiles" -Destination "$configPath/$ver" -Force -Recurse
 
   Write-Output "Launching Blender..."
   Start-Process "$blenderPath/Blender$ver/blender.exe"
 
-  Write-Output "Removing zip file..."
-  Remove-Item -Path "$blenderPath/$package$extension"
+  Write-Output "Removing installation files..."
+  Remove-Item -Path "$blenderPath/$package$extension" -Force
+  Remove-Item -Path "$blenderPath/configFiles.zip" -Force
+  Remove-Item -Path "$blenderPath/configFiles" -Force -Recurse
 
   Write-Output "Installation of Blender$ver$verSub complete..."
   }
-C:\Users\Paul\programs\Blender4.4\4.4
 
 function ConfigPaths {
   $blenderRoot = "$blenderPath/Blender$ver/$ver"
@@ -39,6 +44,7 @@ function ConfigPaths {
   $env:BLENDER_USER_CONFIG = "$blenderRoot/config"
   $env:BLENDER_USER_DATAFILES = "$blenderRoot/datafiles"
   $env:BLENDER_USER_SCRIPTS = "$blenderRoot/scripts"
+  $env:BLENDER_USER_EXTENSIONS = "$blenderRoot/extensions"
 
   }
 
---
+++ git-update.ps1
@@ -0,0 +1,6 @@
+git status
+git add .
+git commit -m "Updated files on $(Get-Date -Format "yyyy-MM-dd HH:mm:ss")"
+git status
+git push
+
